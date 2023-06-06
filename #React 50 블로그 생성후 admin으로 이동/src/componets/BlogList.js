32f461022ecb1deb98aaae1fcd2568e768ca2ff2
whasun <suuneeee@naver.com>
2023-06-06 17:02:07 +0900

    first

---
+++ #React 50 블로그 생성후 admin으로 이동/src/componets/BlogList.js
@@ -0,0 +1,78 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Card from "../componets/Card";
+import { Link, useNavigate } from "react-router-dom";
+import LoadingSpinner from "../componets/LoadingSpinner";
+import PropTypes from "prop-types";
+
+const BlogList = ({ isAdmin }) => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const getPost = () => {
+    axios.get(" http://localhost:3004/posts").then((res) => {
+      //받아온 데이터를 사용하기위해서는 .then를 사용해야하고
+      //.then(함수)  //then(()=>{})
+      //받아온 데이터가 res에 들어간다//response를 줄여서
+      //console.log(res);
+      //console.log(res.data);
+      setPosts(res.data);
+      setLoading(false);
+    });
+  };
+  useEffect(() => {
+    getPost();
+  }, []); //빈배열일때는 한번만 실행한다
+
+  const deleteBlog = (e, id) => {
+    e.stopPropagation();
+    console.log("삭제");
+    axios.delete(`http://localhost:3004/posts/${id}`).then(() => {
+      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
+    });
+  };
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  if (posts.length === 0) {
+    return <div>"blog posts가 발견되지 않습니다."</div>;
+  }
+  return posts
+    .filter((post) => {
+      return isAdmin || post.publish;
+    })
+    .map((post) => {
+      return (
+        <Card
+          key={post.id}
+          title={post.title}
+          onClick={() => {
+            //console.log("실행");
+            navigate(`/blogs/${post.id}`);
+          }}
+        >
+          <div>
+            {isAdmin ? (
+              <button
+                className="btn btn-danger"
+                onClick={(e) => deleteBlog(e, post.id)}
+              >
+                Delete
+              </button>
+            ) : null}
+          </div>
+        </Card>
+      );
+    });
+};
+BlogList.propTypes = {
+  isAdmin: PropTypes.bool,
+};
+BlogList.defaultProps = {
+  isAdmin: false,
+};
+export default BlogList;
---
+++ src/componets/BlogForm.js
@@ -0,0 +1,136 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function BlogForm({ editing }) {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const [originalTitle, setOriginalTitle] = useState("");
+  const [originalBody, setOriginalBody] = useState("");
+  const [publish, setPublish] = useState(false);
+  const [originalPublish, setOriginalPublish] = useState(false);
+
+  const { id } = useParams(); //url에서 parameters(id번호)를 가져온다
+  const onSubmit = () => {
+    //console.log(title, body);
+    if (editing) {
+      axios
+        .patch(`http://localhost:3004/posts/${id}`, {
+          title,
+          body,
+          publish,
+        })
+        .then((res) => {
+          console.log(res);
+          navigate(`/blogs/${id}`);
+        });
+    } else {
+      axios
+        .post("http://localhost:3004/posts", {
+          title,
+          body,
+          publish,
+          createdAt: Date.now(),
+        })
+        .then(() => {
+          //성공하면 then()가 실행된다.
+          navigate("/admin");
+        });
+    }
+  };
+
+  useEffect(() => {
+    if (editing) {
+      axios.get(`http://localhost:3004/posts/${id}`).then((res) => {
+        console.log(res);
+        setTitle(res.data.title);
+        setBody(res.data.body);
+        setOriginalTitle(res.data.title);
+        setOriginalBody(res.data.body);
+        setPublish(res.data.publish);
+        setOriginalPublish(res.data.publish);
+      });
+    }
+  }, [id, editing]); //처음한번만 실행
+
+  const isEdited = () => {
+    //수정이 되었다면 true, 수정이 되지 않았다면 false가 실행된다.
+    return (
+      title !== originalTitle ||
+      body !== originalBody ||
+      publish !== originalPublish
+    );
+  };
+
+  const goBack = () => {
+    if (editing) {
+      navigate(`/blogs/${id}`);
+    } else {
+      navigate(`/blogs`);
+    }
+  };
+
+  const onChagngePublish = (e) => {
+    console.log(e.target.checked);
+    setPublish(e.target.checked);
+  };
+  return (
+    <div className="container">
+      <h2 className="mb-5 mt-3">{editing ? "Edit" : "Create"} a blog post</h2>
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input
+          className="form-control"
+          value={title}
+          onChange={(e) => {
+            //console.log(e.target.value);
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea
+          className="form-control"
+          value={body}
+          rows="10"
+          onChange={(e) => {
+            //console.log(e.target.value);
+            setBody(e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-check mb-3">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={publish}
+          onChange={onChagngePublish}
+        />
+        <label className="form-check-label">publish</label>
+      </div>
+      <button
+        className="btn btn-primary"
+        onClick={onSubmit}
+        disabled={editing && !isEdited()}
+      >
+        {editing ? "Edit" : "Post"}
+      </button>
+      <button className="btn btn-danger ms-2" onClick={goBack}>
+        Cancel
+      </button>
+    </div>
+  );
+}
+
+BlogForm.propTypes = {
+  editing: PropTypes.bool,
+};
+
+BlogForm.defaultProps = {
+  editing: false,
+};
+export default BlogForm;
